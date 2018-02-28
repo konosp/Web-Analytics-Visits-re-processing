c675afafd0d1e1f538bd416bbe6e8ed52cbf1641
konos <papadopoulospk@gmail.com>
2018-02-28 22:11:02 +0000

    File encoding process

--- encoding_update.py
+++ encoding_update.py
@@ -1,14 +1,28 @@
 import gzip
-fp = gzip.open('data/feeds/01-debenhamsdotcomauth_2018-02-06.tsv.gz')
-contents = fp.read() # contents now has the uncompressed bytes of foo.gz
-fp.close()
-u_str = contents.decode('ISO-8859-1').encode('utf-8') # u_str is now a unicode string
-u_str
+import os
+import pdb
 
+initial_path = 'data/feeds'
+final_path = 'data/encoded_feeds'
 
-outfilename = 'data/feeds/encoded.tsv.gz'
-output = gzip.open(outfilename, 'wb')
-try:
-    output.write(u_str)
-finally:
-    output.close()
+files = os.listdir(initial_path)
+
+for file in files:
+    if file.find('.gz') > 0:
+        # pdb.set_trace()
+        print 'Start - encoding: ' + file
+        fp = gzip.open('/'.join([initial_path, file]))
+        contents = fp.read() # contents now has the uncompressed bytes of foo.gz
+        fp.close()
+        u_str = contents.decode('ISO-8859-1').encode('utf-8') # u_str is now a unicode string
+        if not os.path.isdir(final_path):
+            os.mkdir(final_path)
+        
+        outfilename = '/'.join([final_path,'encoded-'+file])
+        output = gzip.open(outfilename, 'wb')
+        try:
+            output.write(u_str)
+            
+        finally:
+            output.close()
+        print 'End - encoding: ' + file
